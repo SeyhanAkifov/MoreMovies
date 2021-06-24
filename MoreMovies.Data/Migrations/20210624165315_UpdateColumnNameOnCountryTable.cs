74142e0b55665155338015821461dcef7042e190
Kabalsky <Kabalsky@protonmail.com>
2021-06-24 18:54:13 +0200

    Update Coulumn name with migration

---
+++ MoreMovies.Data/Migrations/20210624165315_UpdateColumnNameOnCountryTable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoreMovies.Data.Migrations
+{
+    public partial class UpdateColumnNameOnCountryTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Genre",
+                newName: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Genre",
+                newName: "id");
+        }
+    }
+}
